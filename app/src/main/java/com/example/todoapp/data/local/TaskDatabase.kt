fe42c030eab63b8d35c66fb0bc2560f7654a509b
VincenzoCS <nvc05042k4@gmail.com>
2024-12-08 12:01:53 +0700

    commented unused room

--- app/src/main/java/com/example/todoapp/data/local/TaskDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/local/TaskDatabase.kt
@@ -1,30 +1,30 @@
-package com.example.todoapp.data.local
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-
-@Database(entities = [TaskData::class], version = 1)
-abstract class TaskDatabase : RoomDatabase() {
-    abstract fun taskDao(): TaskDao
-
-    companion object {
-        private var instance: TaskDatabase? = null
-        fun getDatabase(context: Context): TaskDatabase {
-            if (instance == null) {
-                synchronized(TaskDatabase::class.java) {
-                    if (instance == null) {
-                        instance = Room.databaseBuilder(
-                            context,
-                            TaskDatabase::class.java,
-                            "tasks.db"
-                        )
-                            .build()
-                    }
-                }
-            }
-            return instance!!
-        }
-    }
-}
+//package com.example.todoapp.data.local
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//
+//@Database(entities = [TaskData::class], version = 1)
+//abstract class TaskDatabase : RoomDatabase() {
+//    abstract fun taskDao(): TaskDao
+//
+//    companion object {
+//        private var instance: TaskDatabase? = null
+//        fun getDatabase(context: Context): TaskDatabase {
+//            if (instance == null) {
+//                synchronized(TaskDatabase::class.java) {
+//                    if (instance == null) {
+//                        instance = Room.databaseBuilder(
+//                            context,
+//                            TaskDatabase::class.java,
+//                            "tasks.db"
+//                        )
+//                            .build()
+//                    }
+//                }
+//            }
+//            return instance!!
+//        }
+//    }
+//}
--- app/src/main/java/com/example/todoapp/di/DatabaseModule.kt
+++ app/src/main/java/com/example/todoapp/di/DatabaseModule.kt
@@ -1,19 +1,19 @@
-package com.example.todoapp.di
-
-import android.app.Application
-import com.example.todoapp.data.local.TaskDatabase
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-
-@Module
-@InstallIn(SingletonComponent::class)
-class DatabaseModule {
-    @Singleton
-    @Provides
-    fun provideDatabase(application: Application): TaskDatabase {
-        return TaskDatabase.getDatabase(application)
-    }
-}
+//package com.example.todoapp.di
+//
+//import android.app.Application
+//import com.example.todoapp.data.local.TaskDatabase
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.components.SingletonComponent
+//import javax.inject.Singleton
+//
+//@Module
+//@InstallIn(SingletonComponent::class)
+//class DatabaseModule {
+//    @Singleton
+//    @Provides
+//    fun provideDatabase(application: Application): TaskDatabase {
+//        return TaskDatabase.getDatabase(application)
+//    }
+//}
